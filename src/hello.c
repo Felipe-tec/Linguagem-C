d0aceacd9ce48a3ca714667a71d28830b6f8ffde
Felipe Frazao <felipefrazao017@gmail.com)>
2024-10-22 17:35:51 -0300

    primeiros códigos em C

---
+++ src/hello.c
@@ -0,0 +1,11 @@
+ // Importação da biblioteca padrão pra o uso de teclado e monotor
+ #include <stdio.h>
+
+ int main(){
+ // para exibir uma mensagem na tela iremos usar
+ // o comando printf(exibir formato)
+ printf("hello, word!\n");
+ // informar a o compilador que o programa
+ // acabou
+  return 0;
+}
---
+++ src/variaveis1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+int n1 = 10;
+int n2 = 20;
+int is = n1 + n2;
+// Imprimir o valor da variável e o 
+// endereço de memória de cada um
+printf("O valor de n1 e %d\n",1);
+// para exibir o endereço de memória iremos usar o
+// comando &
+printf("O endereço de n1 é %p\n", &n1);
+
+printf("O valor de n2 é %d\n",n2);
+printf(" O endereço de n2 é %p\n",&n2);
+
+printf("O valor rs é %d\n",rs);
+printf("O endereço de rs é %p\n",&rs)
+return 0;
+}
